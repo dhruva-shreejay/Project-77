468d8a6317d4bd297715a4eefd20facd5567cc2d
dhruva-shreejay <77469504+dhruva-shreejay@users.noreply.github.com>
2021-05-31 19:11:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Console Project")
+var first_name = "Dhruva"
+var last_name = "Shreejay"
+var full_name = first_name.concat(last_name)
+console.log(full_name)
+var number = 15
+var number_ = 11
+console.log(number + number_)
+console.log(number - number_)
+console.log(number * number_)
+console.log(number/number_)
